d82859067e37f0ad6ad233bbc1e73b2aaaea6bfa
Grzegorz Martowski <gmartowski@pgs-soft.com>
2019-11-08 10:36:30 +0100

    ST-381 Remove unused code

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,14 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const webpack = require('webpack');
-const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
-  mode: 'production',
-  plugins: [
-    new webpack.NormalModuleReplacementPlugin(
-      /^\.\.\/\.\.\/environments\/environment/,
-      '../../environments/environment.prod'
-    ),
-    new webpack.NormalModuleReplacementPlugin(/^\.\/environments\/environment/, './environments/environment.prod'),
-  ]
+    mode: 'production',
+    plugins: [
+        new webpack.NormalModuleReplacementPlugin(
+            /^\.\.\/\.\.\/environments\/environment/,
+            '../../environments/environment.prod'
+        ),
+        new webpack.NormalModuleReplacementPlugin(/^\.\/environments\/environment/, './environments/environment.prod')
+    ]
 });
